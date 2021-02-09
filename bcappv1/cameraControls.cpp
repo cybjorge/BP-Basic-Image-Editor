e8ebff96433d47dca8a966c2c0755b5302fed426
cybjorge <sambal258@gmail.com>
2021-02-09 19:31:42 +0100

    closed file stream

--- bcappv1/cameraControls.cpp
+++ bcappv1/cameraControls.cpp
@@ -9,14 +9,18 @@ int check()
 {	//check whether camera is connected
 	FILE* fv0;
 	FILE* fv1;
-
 	fv0 = (fopen("/dev/video0", "r"));
+
 	fv1 = (fopen("/dev/video1", "r"));
 	if (!fv0 && !fv1) {
 		cout << "No camera is connected \n";
+		fclose(fv0);
+		fclose(fv1);
 		return -1;
 	}
 	cout << "Camera is connected \n";
+	fclose(fv0);
+	fclose(fv1);
 	return 0;
 
 	/*if (system("v4l2-ctl --list-devices")!=0) {
