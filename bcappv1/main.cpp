586ef76824ea216758ce9c12ff72954b8e9086de
cybjorge <sambal258@gmail.com>
2021-06-03 16:50:55 +0200

    main cycle

--- bcappv1/main.cpp
+++ bcappv1/main.cpp
@@ -29,78 +29,78 @@ int main(int argc, char* argv[])
     //open camera;
     
     
-    //int camera_fd = open("/dev/video0", O_RDWR);
-    //if (camera_fd > 0) {
-    //    //prepare camera
-    //    capability(camera_fd);
-    //    set_r_f(camera_fd);
-    //    set_buffer(camera_fd);
+    int camera_fd = open("/dev/video0", O_RDWR);
+    if (camera_fd > 0) {
+        //prepare camera
+        capability(camera_fd);
+        set_r_f(camera_fd);
+        set_buffer(camera_fd);
 
-    //    int i = 0;
-    //    cout << "calibration\n" << flush;
-    //    //auto focus calibration
-    //    while (i < CALIBRATION_REPEAT) {
-    //        make_frame(camera_fd, i, CALIBRATION);
-    //        i++;
-    //    }
-    //    cout << "calibration finished\n";
-    //    //calibration ended, caputre 5 images
-    //    for (int capture_number = 0; capture_number < 5; capture_number++) {
-    //        make_frame(camera_fd, i, CALIBRATION);
-    //        //retrieve adress and size of image
-    //        uint8_t* buffer = return_buf();
-    //        size_t size = return_buf_size();
+        int i = 0;
+        cout << "calibration\n" << flush;
+        //auto focus calibration
+        while (i < CALIBRATION_REPEAT) {
+            make_frame(camera_fd, i, CALIBRATION);
+            i++;
+        }
+        cout << "calibration finished\n";
+        //calibration ended, caputre 5 images
+        for (int capture_number = 0; capture_number < 5; capture_number++) {
+            make_frame(camera_fd, i, CALIBRATION);
+            //retrieve adress and size of image
+            uint8_t* buffer = return_buf();
+            size_t size = return_buf_size();
 
-    //        Image img(buffer, size); //create an instance of the image
-    //        //Image img("/home/pi/projects/bcappv1/a.jpg");
-    //        Histogram h;
-    //        
-    //        img.write("cap.jpg");
-    //        img.write("before.jpg");
-    //        /* preprocessing operations */
-    //        img.median_filter();
-    //        img.grayscale();
-    //        h = img.histogram();
-    //        h.statistics();
-    //       
-    //        int brightness = (h.min_index + h.max_index) / 2;
-    //        img.adjustBrightness(brightness);
-    //        
-    //        h = img.histogram();
-    //        h.statistics();
-    //       
-    //        img.write("bc.jpg");
-    //        h.imagesize = img.size;
-    //        h = img.treshold(h);
-    //        
-    //       if (h.binary_treshold[0] > h.binary_treshold[1]) {
-    //            Image inverted = img;
-    //            img.invert();
-    //            //inverted.write("inverted.jpg");
-    //            h = img.histogram();
-    //       }
-    //        
-    //        img.median_filter();
-    //        img.histogram_equalisation(h);
-    //         /*this creates names for output images*/
-    //        string numname = "capture" + to_string(capture_number) + ".jpg";
-    //        const char* n = numname.c_str();
+            Image img(buffer, size); //create an instance of the image
+            //Image img("/home/pi/projects/bcappv1/a.jpg");
+            Histogram h;
+            
+            img.write("cap.jpg");
+            img.write("before.jpg");
+            /* preprocessing operations */
+            img.median_filter();
+            img.grayscale();
+            h = img.histogram();
+            h.statistics();
+           
+            int brightness = (h.min_index + h.max_index) / 2;
+            img.adjustBrightness(brightness);
+            
+            h = img.histogram();
+            h.statistics();
+           
+            img.write("bc.jpg");
+            h.imagesize = img.size;
+            h = img.treshold(h);
+            
+           if (h.binary_treshold[0] > h.binary_treshold[1]) {
+                Image inverted = img;
+                img.invert();
+                //inverted.write("inverted.jpg");
+                h = img.histogram();
+           }
+            
+            img.median_filter();
+            img.histogram_equalisation(h);
+             /*this creates names for output images*/
+            string numname = "capture" + to_string(capture_number) + ".jpg";
+            const char* n = numname.c_str();
 
-    //        img.write(n);
-    //    }
+            img.write(n);
+        }
 
-    //    stop_stream(camera_fd);
-    //    close(camera_fd);
-    //}
+        stop_stream(camera_fd);
+        close(camera_fd);
+    }
     //
-    Image img("/home/pi/projects/bcappv1/cap.jpg");
-    Histogram h;
+    //Image img("/home/pi/projects/bcappv1/cap.jpg");
+    //Histogram h;
 
-    img.grayscale();
-    h = img.histogram();
-    img.write("grayscaled.jpg");
-    img.treshold(128);
-    img.write("treshold.jpg");
+    //img.grayscale();
+    //h = img.histogram();
+    //img.write("grayscaled.jpg");
+    //img.treshold(128);
+    //img.write("treshold.jpg");
     //Image inv = img;
     //Image eq = img;
     //Image jas = img;
